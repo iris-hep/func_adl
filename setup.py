426e7ce27c813159814913968bd66285f029f328
Gordon Watts <gordon@gordonandpaula.net>
2019-11-30 19:09:52 -0800

    Get around the crazy setup requriements
    Should get CI working again

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 # Need setuptools even though it isn't used - loads some plugins.
 from setuptools import find_packages  # noqa: F401
 from distutils.core import setup
-from version_info import version_func_adl_ast, version_func_adl
 
 # Use the readme as the long description.
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name="func_adl.ast",
-      version=version_func_adl_ast,
-      packages=['func_adl/ast'],
+setup(name="func_adl_test",
+      version='0.0.1a1',
+      packages=['func_adl', 'func_adl/ast'],
       scripts=[],
-      description="Functional Analysis Description Language - Backend AST Manipulation Packages",
+      description="Functional Analysis Description Language - For Testing Only",
       long_description=long_description,
       long_description_content_type="text/markdown",
       author="G. Watts (IRIS-HEP/UW Seattle)",
@@ -21,7 +20,7 @@ setup(name="func_adl.ast",
       url="https://github.com/iris-hep/func_adl",
       license="TBD",
       test_suite="tests",
-      install_requires=[f"func_adl=={version_func_adl}"],
+      install_requires=[],
       setup_requires=["pytest-runner"],
       tests_require=["pytest>=3.9"],
       classifiers=[
