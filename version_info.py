3a385b170a97d5ee84cc0236ab0a2738ccd8a82d
Gordon Watts <gwatts@uw.edu>
2020-01-23 23:49:40 -0800

    Update version number
    Move to a18
    
    #26

--- version_info.py
+++ version_info.py
@@ -1,5 +1,5 @@
 # Contains version numbers for the two packages this repo will make
 
-linked_version = "1.0.0a17"
+linked_version = "1.0.0a18"
 version_func_adl = linked_version
 version_func_adl_ast = linked_version
