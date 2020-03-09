8cb6df5176adc486953d93d2a03d339cabe5cfa5
Gordon Watts <gwatts@uw.edu>
2020-03-09 09:24:52 -0700

    Bump version number

--- .vscode/push-to-pypi.ps1
+++ .vscode/push-to-pypi.ps1
@@ -1,4 +1,9 @@
-Remove-Item ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./build/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse -Confirm:$false ./*.egg-info
 python setup_func_adl.py sdist bdist_wheel
 python setup_func_adl_ast.py sdist bdist_wheel
 twine upload dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./dist/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./build/*
+Remove-Item -ErrorAction SilentlyContinue -Recurse -Confirm:$false ./*.egg-info
