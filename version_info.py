e97640173991c33c3051bbfa53e1ad2c3b164b87
Gordon Watts <gwatts@uw.edu>
2020-03-10 19:55:16 -0700

    Bump to version 21

--- version_info.py
+++ version_info.py
@@ -1,5 +1,5 @@
 # Contains version numbers for the two packages this repo will make
 
-linked_version = "1.0.0a20"
+linked_version = "1.0.0a21"
 version_func_adl = linked_version
 version_func_adl_ast = linked_version
