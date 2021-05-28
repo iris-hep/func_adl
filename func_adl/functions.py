60cb8eabfe7bc14fafe47d376c39608c4af07acb
Baidyanath Kundu <kundubaidya99@gmail.com>
2021-05-28 22:23:15 +0530

    Add Range (#66)
    * Add Range function
    
    Work all done by @sudo-panda

---
+++ func_adl/functions.py
@@ -0,0 +1,24 @@
+import ast
+
+from .object_stream import ObjectStream
+
+from typing import Union, cast
+
+def Range(lower_bound: Union[str, int, ast.AST], upper_bound: Union[str, int, ast.AST]) -> ObjectStream:
+    r"""
+    Given the lower and upper bound return an object with the range of numbers, similar to python range
+    
+    Arguments:
+
+        lower_bound     The start of the range
+        upper_bound     The end of the range
+
+    Return:
+
+        A new ObjectStream that contains the range of numbers
+
+    """
+
+    return ObjectStream(function_call("Range",
+                                          [cast(ast.AST, as_ast(lower_bound)),
+                                           cast(ast.AST, as_ast(upper_bound))]))
