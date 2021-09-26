e8db4f9439ec13cc5fc4329f0fb741f51772deb7
Gordon Watts <gordon@gordonandpaula.net>
2021-09-26 11:48:21 +0200

    Method replacement
    * Fills in default arguments
    * Assures required arguments are there

--- func_adl/type_based_replacement.py
+++ func_adl/type_based_replacement.py
@@ -1,7 +1,9 @@
-from typing import Any, Tuple, TypeVar
-from .object_stream import ObjectStream
 import ast
+import copy
+import inspect
+from typing import Any, Dict, Tuple, TypeVar, Union
 
+from .object_stream import ObjectStream
 
 T = TypeVar('T')
 S = TypeVar('S')
@@ -9,4 +11,72 @@ S = TypeVar('S')
 
 def remap_by_types(o_stream: ObjectStream[T], var_name: str, var_type: Any, a: ast.AST) \
         -> Tuple[ObjectStream[T], ast.AST]:
-    return o_stream, a
+
+    class type_transformer(ast.NodeTransformer):
+        def __init__(self, o_stream: ObjectStream[T]):
+            self._stream = o_stream
+            self._found_types: Dict[Union[str, object], type] = {
+                var_name: var_type
+            }
+
+        @property
+        def stream(self) -> ObjectStream[T]:
+            return self._stream
+
+        def process_method_call(self, node: ast.Call, obj_type: type) -> ast.AST:
+
+            # Make reference copies that we'll populate as we go
+            r_node = node
+
+            # Deal with default arguments and the method signature
+            assert isinstance(r_node.func, ast.Attribute)
+            call_method = getattr(obj_type, r_node.func.attr, None)
+            if call_method is None:
+                return r_node
+
+            sig = inspect.signature(call_method)
+            i_arg = 0
+            arg_array = list(r_node.args)
+            for param in sig.parameters.values():
+                if param.name != 'self':
+                    if len(arg_array) <= i_arg:
+                        if param.default is param.empty:
+                            raise ValueError(f'Argument {param.name} on method {r_node.func.attr} '
+                                             f'on class {obj_type.__name__} is required.')
+                        a = ast.Constant(param.default)
+                        arg_array.append(a)
+            if len(arg_array) != len(r_node.args):
+                r_node = copy.copy(r_node)
+                r_node.args = arg_array
+
+            # See if someone wants to process the call
+            attr = getattr(obj_type, '_func_adl_type_info')
+            if attr is not None:
+                r_stream, r_node = attr(self.stream, r_node)
+                assert isinstance(r_node, ast.AST)
+                self._stream = r_stream
+
+            # And if we have a return annotation, then we should record it!
+            self._found_types[r_node] = sig.return_annotation
+
+            return r_node
+
+        def visit_Call(self, node: ast.Call) -> ast.AST:
+            t_node = self.generic_visit(node)
+            assert isinstance(t_node, ast.Call)
+            if isinstance(t_node.func, ast.Attribute):
+                # Do we know the type of the value?
+                found_type = self._found_types.get(t_node.func.value, None)
+                if found_type is not None:
+                    t_node = self.process_method_call(t_node, found_type)
+            return t_node
+
+        def visit_Name(self, node: ast.Name) -> ast.Name:
+            if node.id in self._found_types:
+                self._found_types[node] = self._found_types[node.id]
+            return node
+
+    tt = type_transformer(o_stream)
+    r_a = tt.visit(a)
+
+    return tt.stream, r_a
--- tests/test_type_based_replacement.py
+++ tests/test_type_based_replacement.py
@@ -1,7 +1,8 @@
 import ast
 from func_adl.type_based_replacement import remap_by_types
-from typing import Iterable
+from typing import Iterable, Tuple, TypeVar
 from func_adl import ObjectStream
+import pytest
 
 
 class Track:
@@ -23,44 +24,117 @@ class Jet:
         ...
 
 
+T = TypeVar('T')
+
+
+def add_met_info(s: ObjectStream[T], a: ast.AST) -> Tuple[ObjectStream[T], ast.AST]:
+    s_update = s.MetaData({'j': 'pxy stuff'})
+    return s_update, a
+
+
 class met:
+    _func_adl_type_info = add_met_info
+
     def pxy(self) -> float:
         ...
 
 
+def add_collection(s: ObjectStream[T], a: ast.Call) -> Tuple[ObjectStream[T], ast.AST]:
+    '''Add a collection to the object stream
+    '''
+    assert isinstance(a.func, ast.Attribute)
+    if a.func.attr == 'Jets':
+        s_update = s.MetaData({'j': 'stuff'})
+        return s_update, a
+    else:
+        return s, a
+
+
 class Event:
-    __func_adl_annotations = {
-        'Jets': {
-            'metadata': {
-                'j': 'stuff',
-            }
-        }
-    }
+    _func_adl_type_info = add_collection
 
     def Jets(self, bank: str = 'default') -> Iterable[Jet]:
         ...
 
-    def MET(self, ) -> met:
+    def Jets_req(self, bank_required: str) -> Iterable[Jet]:
+        ...
+
+    def MET(self) -> met:
+        ...
+
+    def MET_noreturntype(self):
+        ...
+
+    def Tracks(self) -> Iterable[Track]:
         ...
 
 
 def test_collection():
     'A simple collection'
-    s = ast.parse("e.Jets()")
-    objs = ObjectStream[Event](ast.Name(id='e'))
+    s = ast.parse("e.Jets('default')")
+    objs = ObjectStream[Event](ast.Name(id='e', ctx=ast.Load()))
 
     new_objs, new_s = remap_by_types(objs, 'e', Event, s)
 
-    assert ast.dump(new_s) == ast.parse("e.Jets('default')")
-    assert ast.dump(new_objs.query_ast) == ast.parse("MetaData(e, {'j': 'stuff'}")
+    assert ast.dump(new_s) == ast.dump(ast.parse("e.Jets('default')"))
+    assert ast.dump(new_objs.query_ast) \
+        == ast.dump(ast.parse("MetaData(e, {'j': 'stuff'})").body[0].value)  # type: ignore
+
+
+def test_required_arg():
+    'A simple collection'
+    s = ast.parse("e.Jets_req()")
+    objs = ObjectStream[Event](ast.Name(id='e', ctx=ast.Load()))
+
+    with pytest.raises(ValueError) as e:
+        remap_by_types(objs, 'e', Event, s)
+
+    assert 'bank_required' in str(e)
+
+
+def test_collection_with_default():
+    'A simple collection'
+    s = ast.parse("e.Jets()")
+    objs = ObjectStream[Event](ast.Name(id='e', ctx=ast.Load()))
+
+    new_objs, new_s = remap_by_types(objs, 'e', Event, s)
+
+    assert ast.dump(new_s) == ast.dump(ast.parse("e.Jets('default')"))
+    assert ast.dump(new_objs.query_ast) \
+        == ast.dump(ast.parse("MetaData(e, {'j': 'stuff'})").body[0].value)  # type: ignore
 
 
 def test_method_on_collection():
-    'A simple collection'
+    'Call a method that requires some special stuff on a returend object'
     s = ast.parse("e.MET().pxy()")
-    objs = ObjectStream[Event](ast.Name(id='e'))
+    objs = ObjectStream[Event](ast.Name(id='e', ctx=ast.Load()))
 
     new_objs, new_s = remap_by_types(objs, 'e', Event, s)
 
-    assert ast.dump(new_s) == ast.parse("e.MET().pxy()/1000.0")
-    assert ast.dump(new_objs.query_ast) == ast.parse("MetaData(e, {'j': 'pxy stuff'}")
+    assert ast.dump(new_s) == ast.dump(ast.parse("e.MET().pxy()"))
+    assert ast.dump(new_objs.query_ast) \
+        == ast.dump(ast.parse("MetaData(e, {'j': 'pxy stuff'})").body[0].value)  # type: ignore
+
+
+def test_method_with_no_return_type():
+    'A simple collection'
+    s = ast.parse("e.MET_noreturntype().pxy()")
+    objs = ObjectStream[Event](ast.Name(id='e', ctx=ast.Load()))
+
+    new_objs, new_s = remap_by_types(objs, 'e', Event, s)
+
+    assert ast.dump(new_s) == ast.dump(ast.parse("e.MET_noreturntype().pxy()"))
+    assert ast.dump(new_objs.query_ast) \
+        == ast.dump(ast.parse("e").body[0].value)  # type: ignore
+
+
+def test_bogus_method():
+    'A method that is not typed'
+    s = ast.parse("e.Jetsss('default')")
+    objs = ObjectStream[Event](ast.Name(id='e', ctx=ast.Load()))
+
+    new_objs, new_s = remap_by_types(objs, 'e', Event, s)
+
+    assert ast.dump(new_s) == ast.dump(ast.parse("e.Jetsss('default')"))
+    assert ast.dump(new_objs.query_ast) \
+        == ast.dump(ast.parse("e").body[0].value)  # type: ignore
