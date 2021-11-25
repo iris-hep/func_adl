5feadb0ccf0efc17c554b114378fa7e7ed6a626f
Gordon Watts <gordon@gordonandpaula.net>
2021-11-25 14:50:08 +0100

    Re-write for use in 3.7

--- func_adl/util_types.py
+++ func_adl/util_types.py
@@ -1,4 +1,24 @@
-from typing import Any, Type, get_args
+from typing import Any, Type
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import get_args
+else:
+    from typing import _AnnotatedAlias, GenericAlias, _GenericAlias, _is_param_expr
+    import types
+    import collections
+    def get_args(tp):
+        if isinstance(tp, _AnnotatedAlias):
+            return (tp.__origin__,) + tp.__metadata__
+        if isinstance(tp, (_GenericAlias, GenericAlias)):
+            res = tp.__args__
+            if (tp.__origin__ is collections.abc.Callable
+                    and not (len(res) == 2 and _is_param_expr(res[0]))):
+                res = (list(res[:-1]), res[-1])
+            return res
+        if isinstance(tp, types.UnionType):
+            return tp.__args__
+        return ()
 
 
 def unwrap_iterable(t: Type) -> Type:
