8f0c1cd001173e0e19a3c92bd2776ed5623cbff6
Gordon Watts <gordon@gordonandpaula.net>
2021-11-25 15:01:38 +0100

    Getting desperate to get working in 3.7

--- func_adl/util_types.py
+++ func_adl/util_types.py
@@ -4,20 +4,21 @@ import sys
 if sys.version_info >= (3, 8):
     from typing import get_args
 else:
-    from typing import GenericAlias, _GenericAlias, _is_param_expr
-    import types
-    import collections
+    # from typing import GenericAlias, _GenericAlias, _is_param_expr
+    # import types
+    # import collections
 
     def get_args(tp):
-        if isinstance(tp, (_GenericAlias, GenericAlias)):
-            res = tp.__args__
-            if (tp.__origin__ is collections.abc.Callable
-                    and not (len(res) == 2 and _is_param_expr(res[0]))):
-                res = (list(res[:-1]), res[-1])
-            return res
-        if isinstance(tp, types.UnionType):
-            return tp.__args__
-        return ()
+        return tp.__args__
+        # if isinstance(tp, (_GenericAlias, GenericAlias)):
+        #     res = tp.__args__
+        #     if (tp.__origin__ is collections.abc.Callable
+        #             and not (len(res) == 2 and _is_param_expr(res[0]))):
+        #         res = (list(res[:-1]), res[-1])
+        #     return res
+        # if isinstance(tp, types.UnionType):
+        #     return tp.__args__
+        # return ()
 
 
 def unwrap_iterable(t: Type) -> Type:
