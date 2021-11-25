50c7a9bbc8c0bff92b901895993d432ca1be115d
Gordon Watts <gordon@gordonandpaula.net>
2021-11-25 18:10:13 +0100

    Add better type tracking
    We need to track back up the inherritance tree...

--- func_adl/util_types.py
+++ func_adl/util_types.py
@@ -9,10 +9,31 @@ else:  # pragma: no cover
         return tp.__args__
 
 
+def is_iterable(t: Type) -> bool:
+    'Is this type iterable?'
+    if getattr(t, '_name', None) == 'Iterable':
+        return True
+    return False
+
+
+def get_inherited(t: Type) -> Type:
+    'Get the inherited type'
+    base_classes = getattr(t, '__orig_bases__', None)
+    if base_classes is None:
+        return Any
+
+    return t.__orig_bases__[0]
+
+
 def unwrap_iterable(t: Type) -> Type:
     'Unwrap an iterable type'
+    # Try to find an iterable in the history somehow
+    while (t is not Any) and (not is_iterable(t)):
+        t = get_inherited(t)
+
     if t == Any:
         return Any
+
     a = get_args(t)
     assert len(a) == 1
     return a[0]
---
+++ tests/test_util_types.py
@@ -0,0 +1,30 @@
+from typing import Any, Iterable
+
+import pytest
+from func_adl.util_types import unwrap_iterable
+
+
+def test_Any():
+    assert unwrap_iterable(Any) == Any
+
+
+def test_iterable():
+    assert unwrap_iterable(Iterable[int]) == int
+
+
+def test_inherrited():
+    class bogus(Iterable[float]):
+        pass
+
+    assert unwrap_iterable(bogus) == float
+
+
+def test_non_iterable():
+        assert unwrap_iterable(int) == Any
+
+
+def test_non_iterable_obj():
+    class bogus:
+        pass
+
+    assert unwrap_iterable(bogus) == Any
