ad08f6967dd26adc6d766e9efc7166f181adbc33
Gordon Watts <gordon@gordonandpaula.net>
2022-02-13 18:20:02 +0100

    Forgot to format this file correctly

--- setup.py
+++ setup.py
@@ -8,46 +8,54 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 extras_require = {
-    'test': [
-                'pytest', 'pytest-asyncio', 'pytest-cov', 'flake8',
-                'coverage', 'twine', 'wheel', 'astunparse', 'black',
-                'isort'
-            ]
-                 }
-extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
+    "test": [
+        "pytest",
+        "pytest-asyncio",
+        "pytest-cov",
+        "flake8",
+        "coverage",
+        "twine",
+        "wheel",
+        "astunparse",
+        "black",
+        "isort",
+    ]
+}
+extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
 
-setup(name="func_adl_test",
-      version='0.0.1a1',
-      packages=['func_adl', 'func_adl/ast'],
-      scripts=[],
-      description="Functional Analysis Description Language - For Testing Only",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      author="G. Watts (IRIS-HEP/UW Seattle)",
-      author_email="gwatts@uw.edu",
-      maintainer="Gordon Watts (IRIS-HEP/UW Seattle)",
-      maintainer_email="gwatts@uw.edu",
-      url="https://github.com/iris-hep/func_adl",
-      license="TBD",
-      test_suite="tests",
-      install_requires=["make-it-sync"],
-      extras_require=extras_require,
-      classifiers=[
-                   # "Development Status :: 3 - Alpha",
-                   # "Development Status :: 4 - Beta",
-                   # "Development Status :: 5 - Production/Stable",
-                   # "Development Status :: 6 - Mature",
-                   "Intended Audience :: Developers",
-                   "Intended Audience :: Information Technology",
-                   "Programming Language :: Python",
-                   "Programming Language :: Python :: 3.9",
-                   "Programming Language :: Python :: 3.8",
-                   "Programming Language :: Python :: 3.6",
-                   "Programming Language :: Python :: 3.7",
-                   "Topic :: Software Development",
-                   "Topic :: Utilities",
-      ],
-      data_files=[],
-      python_requires='>=3.7, <3.11',
-      platforms="Any",
-      )
+setup(
+    name="func_adl_test",
+    version="0.0.1a1",
+    packages=["func_adl", "func_adl/ast"],
+    scripts=[],
+    description="Functional Analysis Description Language - For Testing Only",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="G. Watts (IRIS-HEP/UW Seattle)",
+    author_email="gwatts@uw.edu",
+    maintainer="Gordon Watts (IRIS-HEP/UW Seattle)",
+    maintainer_email="gwatts@uw.edu",
+    url="https://github.com/iris-hep/func_adl",
+    license="TBD",
+    test_suite="tests",
+    install_requires=["make-it-sync"],
+    extras_require=extras_require,
+    classifiers=[
+        # "Development Status :: 3 - Alpha",
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+        # "Development Status :: 6 - Mature",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+    data_files=[],
+    python_requires=">=3.7, <3.11",
+    platforms="Any",
+)
