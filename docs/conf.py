e322a6c03d134599e3194b88301fe6092dd75ecb
RogerJanusiak <roger22011@gmail.com>
2025-08-04 16:26:23 -0500

    Updated to pass flake8

--- docs/conf.py
+++ docs/conf.py
@@ -6,26 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'FuncADL'
-copyright = '2025 Institute for Research and Innovation in Software for High Energy Physics (IRIS-HEP)'
-author = 'Institute for Research and Innovation in Software for High Energy Physics (IRIS-HEP)'
-release = '3.5.0'
+project = "FuncADL"
+copyright = (
+    "2025 Institute for Research and " "Innovation in Software for High Energy Physics (IRIS-HEP)"
+)
+author = "Institute for Research and Innovation in Software for High Energy Physics (IRIS-HEP)"
+release = "3.5.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
+    "myst_parser",
     "sphinx.ext.doctest",
 ]
 
-#templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-#html_static_path = ['_static']
