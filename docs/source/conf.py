4a06d61900ae589455ba2ac84f32d5d62019e05f
Roger Janusiak <rogerja@uw.edu>
2026-02-10 12:08:01 -0800

    Remove outdated GitHub Actions workflow and add custom page template with dynamic navbar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,10 +21,28 @@ extensions = [
     "sphinx.ext.doctest",
 ]
 
-exclude_patterns = []
+templates_path = ['_templates']
 
+html_css_files = [
+    ('https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css', {'crossorigin': 'anonymous'}),
+    ('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css', {'crossorigin': 'anonymous'}),
+    ('https://tryservicex.org/css/navbar.css', {'crossorigin': 'anonymous'}),
+    ('https://tryservicex.org/css/sphinx.css', {'crossorigin': 'anonymous'}),
+]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_js_files = [
+    ('https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js', 
+        {'integrity': 'sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI', 'crossorigin': 'anonymous'}
+    ),
+]
+
+html_sidebars = {
+    "**": [
+        "sidebar/brand.html",
+        "sidebar/navigation.html",
+        "sidebar/scroll-start.html",
+        "sidebar/scroll-end.html",
+    ]
+}
 
 html_theme = "furo"
