c5566970822463df04e85be0bf7a2bb7bd6d83c3
Gordon Watts <gwatts@uw.edu>
2026-02-12 20:10:52 -0800

    Fix up flake8 errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,18 +22,28 @@ extensions = [
     "sphinx_copybutton",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 html_css_files = [
-    ('https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css', {'crossorigin': 'anonymous'}),
-    ('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css', {'crossorigin': 'anonymous'}),
-    ('https://tryservicex.org/css/navbar.css', {'crossorigin': 'anonymous'}),
-    ('https://tryservicex.org/css/sphinx.css', {'crossorigin': 'anonymous'}),
+    (
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css",
+        {"crossorigin": "anonymous"},
+    ),
+    (
+        "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
+        {"crossorigin": "anonymous"},
+    ),
+    ("https://tryservicex.org/css/navbar.css", {"crossorigin": "anonymous"}),
+    ("https://tryservicex.org/css/sphinx.css", {"crossorigin": "anonymous"}),
 ]
 
 html_js_files = [
-    ('https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js', 
-        {'integrity': 'sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI', 'crossorigin': 'anonymous'}
+    (
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js",
+        {
+            "integrity": "sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI",
+            "crossorigin": "anonymous",
+        },
     ),
 ]
 
