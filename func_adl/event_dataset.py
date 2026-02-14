5a963f518794277f09ea04fd23e73714c0f5af68
Gordon Watts <gwatts@uw.edu>
2026-02-14 01:59:20 -0800

    Add the find dataset function back in

--- func_adl/event_dataset.py
+++ func_adl/event_dataset.py
@@ -1,5 +1,6 @@
+import ast
 from abc import ABC
-from typing import Any, Type, TypeVar
+from typing import Any, Optional, Type, TypeVar
 
 from .object_stream import ObjectStream
 from .util_ast import function_call
@@ -34,3 +35,42 @@ class EventDataset(ObjectStream[T], ABC):
 
     def __str__(self):
         return self.__class__.__name__
+
+
+def find_EventDataset(a: ast.AST) -> ast.Call:
+    r"""
+    Given an input query ast, find the EventDataset `ast` and return it.
+
+    Args:
+        a:      An AST that represents a query
+
+    Returns:
+        The `EventDataset` `ast` at the root of this query. It will not be None.
+
+    Exceptions:
+        If there is more than one `EventDataset` found in the query or if there
+        is no `EventDataset` at the root of the query, then an exception is thrown.
+    """
+
+    class ds_finder(ast.NodeVisitor):
+        def __init__(self):
+            self.ds: Optional[ast.Call] = None
+
+        def visit_Call(self, node: ast.Call):
+            if not isinstance(node.func, ast.Name):
+                return self.generic_visit(node)
+            if node.func.id != "EventDataset":
+                return self.generic_visit(node)
+
+            if self.ds is not None:
+                raise Exception("AST Query has more than one EventDataset in it!")
+            self.ds = node
+            return node
+
+    ds_f = ds_finder()
+    ds_f.visit(a)
+
+    if ds_f.ds is None:
+        raise Exception(f"AST Query has no root EventDataset: {ast.dump(a)}")
+
+    return ds_f.ds
